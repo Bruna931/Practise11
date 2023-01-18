41aa0558ed1e4f8572612c0d4a632abcaf394dc3
Brunilda <brunillda.isaku@invenics.com>
2023-01-18 15:43:26 +0000

    New

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // is the main part to hold everything related to the app.
+      backgroundColor: Colors.amber[300],
+    );
+  }
+} //Hellogit 
